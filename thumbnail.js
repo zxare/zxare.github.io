166cbf01c9a67e1c6fb17a7233560f8efffed820
zxare <100737157+zxare@users.noreply.github.com>
2022-03-02 17:41:53 +0800

    Add files via upload

--- thumbnail.js
+++ thumbnail.js
@@ -1,17 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
-    {
-    id="smart-image"
-}
 let targetImage = document.querySelector("#smart-image");
 targetImage.addEventListener("click", function() {
     alert("点击了图片");
 });
 targetImage.classList.remove("small");
-
-
-
-
-
-
 });
