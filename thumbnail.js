6ba3c53b173df905c0508a9f8b26cf141c54f93c
zxare <100737157+zxare@users.noreply.github.com>
2022-03-04 20:16:34 +0800

    Add files via upload

--- thumbnail.js
+++ thumbnail.js
@@ -1,11 +1,57 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
-let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click", function() {
+ let targetImage = document.querySelector ("#smart-image");
+ targetImage.addEventListener("click", function(){
     if (targetImage.classList.contains("small")) {
         targetImage.classList.remove("small");
     } else {
         targetImage.classList.add("small");
     }
 });
+
+ let targetImage1 = document.querySelector ("#a");
+ targetImage1.addEventListener("click", function(){
+    if (targetImage1.classList.contains("small")) {
+        targetImage1.classList.remove("small");
+    } else {
+        targetImage1.classList.add("small");
+    }
 });
+
+let targetImage2 = document.querySelector ("#b");
+ targetImage2.addEventListener("click", function(){
+    if (targetImage2.classList.contains("small")) {
+        targetImage2.classList.remove("small");
+    } else {
+        targetImage2.classList.add("small");
+    }
+});
+
+let targetImage3 = document.querySelector ("#c");
+ targetImage3.addEventListener("click", function(){
+    if (targetImage3.classList.contains("small")) {
+        targetImage3.classList.remove("small");
+    } else {
+        targetImage3.classList.add("small");
+    }
+});
+
+let targetImage4 = document.querySelector ("#d");
+ targetImage4.addEventListener("click", function(){
+    if (targetImage4.classList.contains("small")) 
+    {
+        targetImage4.classList.remove("small");
+    } 
+    else 
+    {
+        targetImage4.classList.add("small");
+    }
+});
+
+
+
+
+
+});
+
+
